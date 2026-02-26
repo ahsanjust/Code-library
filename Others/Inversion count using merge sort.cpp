e1be6d5030cea4b23ee2a1cb7b955f65cb83cc3c
Ahsanul Haque <118393606+ahsanjust@users.noreply.github.com>
2026-02-26 19:23:32 +0600

    Update Inversion count using merge sort.cpp

--- Others/Inversion count using merge sort.cpp
+++ Others/Inversion count using merge sort.cpp
@@ -1,17 +1,21 @@
-int64_t inv_cnt(vector<int> &a) {
-	if (a.size() <= 1) return 0;
-	int n = a.size();
-	vector<int> left(a.begin(), a.begin() + n / 2);
-	vector<int> right(a.begin() + n / 2, a.end());
-	int64_t ret = inv_cnt(left) + inv_cnt(right);
-	for (int i = 0, j = 0, k = 0; i < (int)left.size() || j < (int)right.size();) {
-		if (i == (int)left.size()) a[k++] = right[j++];
-		else if (j == (int)right.size()) a[k++] = left[i++];
-		else if (left[i] <= right[j]) a[k++] = left[i++];
-		else {
-			ret += (int)left.size() - i;
-			a[k++] = right[j++];
-		}
-	}
-	return ret;
+int64_t inv_cnt(vector<int> a) {
+  int n = a.size();
+  vector<int> tmp(n);
+  function<int64_t(int,int)> solve = [&](int l, int r) -> int64_t {
+    if (r - l <= 1) return 0;
+    int m = (l + r) / 2;
+    int64_t inv = solve(l, m) + solve(m, r);
+    int i = l, j = m, k = l;
+    while (i < m || j < r) {
+      if (j == r || (i < m && a[i] <= a[j])) {
+        tmp[k++] = a[i++];
+      } else {
+        inv += m - i;
+        tmp[k++] = a[j++];
+      }
+    }
+    for (int t = l; t < r; ++t) a[t] = tmp[t];
+    return inv;
+  };
+  return solve(0, n);
 }
